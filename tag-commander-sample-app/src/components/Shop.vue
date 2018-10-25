5e551f586e57077fd790a180f0411c137fcb4fd1
Fabien Walesch <fabien.walesch@gmail.com>
2018-10-25 15:07:19 +0200

    Item constructor done

--- tag-commander-sample-app/src/components/Shop.vue
+++ tag-commander-sample-app/src/components/Shop.vue
@@ -41,6 +41,15 @@
 
 <script>
 
+class Item {
+    constructor(id, name, price, quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
 let pageItemName = {
     props: ['product'],
     name: 'PageItemName',
