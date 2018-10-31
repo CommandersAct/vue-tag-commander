ea71c7c82025167a554438759a92b4e8c3beaf02
Fabien Walesch <fabien.walesch@gmail.com>
2018-10-31 17:56:51 +0100

    commit 31/10/2018//17h56

--- src/index.js
+++ src/index.js
@@ -5,5 +5,14 @@ import VueAxios from 'vue-axios'
 Vue.use(VueAxios, axios)
 
 export default class TC_Wrapper {
-    
-};
+    constructor() {
+        this.logger = window.console;
+        this.tcContainers = [];
+        this.isTracking = false;
+        this.instance = null;
+    }
+}
+
+export function withTracker(WrappedComponent, options = {}) {
+
+}
