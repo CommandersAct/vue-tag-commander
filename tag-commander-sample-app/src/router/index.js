fc3de314252776c4860c7b5c85c201ff8d8deff8
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-03-25 17:34:31 +0100

    nothing update

--- tag-commander-sample-app/src/router/index.js
+++ tag-commander-sample-app/src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Index from '@/components/Index';
-import Shop from '@/components/Shop';
-import Dashboard from '@/components/Dashboard';
+import Vue from "vue";
+import Router from "vue-router";
+import Index from "@/components/Index";
+import Shop from "@/components/Shop";
+import Dashboard from "@/components/Dashboard";
 
-import  TC_Wrapper,{WithTracker} from "vue-tag-commander";
+import TC_Wrapper, { WithTracker } from "vue-tag-commander";
 
 const wrapper = TC_Wrapper.getInstance();
 wrapper.setDebug(true);
@@ -13,48 +13,50 @@ wrapper.setDebug(true);
 wrapper.trackRoutes(true);
 
 // // to set the TagCommander container provide the id
-wrapper.addContainer('container_head', '../../static/tag-commander-head.js', 'head');
-wrapper.addContainer('container_body', '../../static/tag-commander-body.js', 'body');
+wrapper.addContainer(
+  "container_head",
+  "../../static/tag-commander-head.js",
+  "head"
+);
+wrapper.addContainer(
+  "container_body",
+  "../../static/tag-commander-body.js",
+  "body"
+);
 
-
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  routes: [{
-      path: '/',
-      name: 'index',
-      component:
-      WithTracker(Index,
-      {tcReloadOnly:[
-        {ids :'4056', idc: '12'}
-      ]})
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "index",
+      component: WithTracker(Index, {
+        tcReloadOnly: [{ ids: "4056", idc: "12" }]
+      })
     },
 
     {
-      path: '/shop',
-      name: 'shop',
-      component:
-      WithTracker(Shop,
-      {tcReloadOnly: [
-        {ids :'4056', idc: '12'}, 
-        {ids :'4056', idc: '11', options:["datastorage", "deduplication"]}
-      ]
-      })
-    },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: 
-      WithTracker(Dashboard,
-        {tcReloadOnly:[
-        {ids :'4056', idc: '12'}
+      path: "/shop",
+      name: "shop",
+      component: WithTracker(Shop, {
+        tcReloadOnly: [
+          { ids: "4056", idc: "12" },
+          { ids: "4056", idc: "11", options: ["datastorage", "deduplication"] }
         ]
       })
     },
     {
-      path: '*',
-      redirect: '/'
+      path: "/dashboard",
+      name: "dashboard",
+      component: WithTracker(Dashboard, {
+        tcReloadOnly: [{ ids: "4056", idc: "12" }]
+      })
+    },
+    {
+      path: "*",
+      redirect: "/"
     }
   ]
-})
+});
