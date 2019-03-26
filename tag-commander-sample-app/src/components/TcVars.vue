e3a6400add197c7a8acd1b88988d45dd0ee39206
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-03-26 17:50:55 +0100

    auto indent composant

--- tag-commander-sample-app/src/components/TcVars.vue
+++ tag-commander-sample-app/src/components/TcVars.vue
@@ -1,19 +1,18 @@
 <script>
-
 import TC_Wrapper from "vue-tag-commander";
 
 let wrapper = TC_Wrapper.getInstance();
 
 export default {
-  name : 'TcVars',
+  name: "TcVars",
   props: {
     env_language: {
-    type:String,
-    default:'fr'
+      type: String,
+      default: "fr"
     },
     env_template: {
-    type:String,
-    default:"super_shop"
+      type: String,
+      default: "super_shop"
     }
   },
   mounted() {
@@ -22,5 +21,5 @@ export default {
   render() {
     return null;
   }
-}
+};
 </script>
