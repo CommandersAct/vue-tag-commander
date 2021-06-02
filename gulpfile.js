95cad33bd99b0677bccadd1c6573d81191aeeefe
Romain Laï-King <romain.laiking@commandersact.com>
2021-06-02 02:43:34 +0200

    CJS build

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ var gulp = require('gulp');
 var minify = require('gulp-minify');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
- 
-gulp.task('scripts', function() {
-  return gulp.src('./lib/*.js')
-    .pipe(gulp.dest('./dist/'));
+var rollup = require('@rollup/stream');
+var source = require('vinyl-source-stream');
+
+
+gulp.task('scripts', function () {
+    return gulp.src('./lib/*.js')
+        .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('build-es5', function() {
+gulp.task('build-es5', function () {
     console.log("building es5 lib");
     // converting to ES5
     gulp.src('src/*.js')
@@ -16,54 +19,50 @@ gulp.task('build-es5', function() {
             presets: ['env', 'es2015', 'vue']
         }))
         .pipe(minify({
-        ext:{
-            min:'.es5.min.js'
-        },
-        exclude: ['tasks'],
-        noSource: true
-    }))
-    .pipe(concat('vue-tag-commander.es5.min.js'))
-    .pipe(gulp.dest('dist'));
+            ext: {
+                min: '.es5.min.js'
+            },
+            exclude: ['tasks'],
+            noSource: true
+        }))
+        .pipe(concat('index.es5.min.js'))
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-es6', function() {
+gulp.task('build-es6', function () {
     console.log("building es6 lib");
     gulp.src('src/*.js')
         .pipe(minify({
-        ext:{
-            min:'.es6.min.js'
-        },
-        exclude: ['tasks'],
-        noSource: true
-    }))
-    .pipe(concat('vue-tag-commander.es6.min.js'))
-    .pipe(gulp.dest('dist'));
+            ext: {
+                min: '.es6.min.js'
+            },
+            exclude: ['tasks'],
+            noSource: true
+        }))
+        .pipe(concat('index.es6.min.js'))
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-umd', function() {
+gulp.task('build-cjs', function () {
     console.log("building umd lib");
-    // converting to ES5
 
-    gulp.src('package.umd.json')
-    .pipe(concat('package.json'))
-    .pipe(gulp.dest('umd'));
+    rollup({
+        input: './src/index.js',
+        output: { format: 'cjs' },
+      })
+  
+      // give the file the name you want to output with
+      .pipe(source('index.js'))
+  
+      // and output to ./dist/app.js as normal.
+      .pipe(gulp.dest('./cjs'));
 
-    gulp.src('src/*.js')
-    .pipe(babel({
-        presets: ['env', 'es2015', 'vue'],
-        plugins: ["transform-es2015-modules-umd"]
-    }))
-    .pipe(minify({
-    ext:{
-        min:'.umd.min.js'
-    },
-    exclude: ['tasks'],
-    noSource: true
-}))
-.pipe(concat('index.js'))
-.pipe(gulp.dest('umd'));
+
+    gulp.src('package.cjs.json')
+        .pipe(concat('package.json'))
+        .pipe(gulp.dest('cjs'));
 
 });
 
 
-gulp.task('default', ['build-es5', 'build-es6', 'build-umd']);
+gulp.task('default', ['build-es5', 'build-es6', 'build-cjs']);
